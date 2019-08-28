9f91d020fd2539501e6b8e666ece6d115cf29847
GJTing666 <43943366+gjTing@users.noreply.github.com>
2019-08-28 23:40:36 +0800

    完善帮助中心静态界面

--- 洋货码头/js/member.js
+++ 洋货码头/js/member.js
@@ -1,16 +1,13 @@
 $('.liStyle').click(function() {
-		var parent = $(this).parent();
-		if(parent.hasClass('on')) {
-			parent.removeClass('on');
-		}else{
+	var parent = $(this).parent();
+	if (parent.hasClass('on')) {
+		parent.removeClass('on');
+	} else {
 		parent.addClass('on');
 		parent.siblings().removeClass('on');
 		// 父级的兄弟姐妹
 		parent.siblings().find('.order-lists').slideUp();
-		}
-		// 显示与隐藏的切换
-		$(this).next().slideToggle(300);
-	})
-	
-
-
+	}
+	// 显示与隐藏的切换
+	$(this).next().slideToggle(300);
+})
